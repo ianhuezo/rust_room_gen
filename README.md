dfdca0333abb3dcab4fa92e6fa5f0f7b72a3ad62
Ian Huezo <ianhuezo@gmail.com>
2020-06-03 21:48:52 -0700

    commits and to github

---
+++ README.md
@@ -0,0 +1,5 @@
+### A random rust project for maze generation to get acquainted with the language
+
+---
+
+I will eventually make a lib.rs and move things around, but that will be later
--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@
 //   if any of the sides are a length of <= one when it's reduced, don't make the room at all, throw an error
 //if new room in certain direction is unsuccessful, the attempt to create a hallway is unsuccessful and removed from room hallway potentials
 
+use std::error::Error;
 use std::fmt::Debug;
 
 #[derive(Debug)]
@@ -26,13 +27,13 @@ pub struct Position {
 }
 
 impl Position {
-    fn new(row: usize, col: usize) -> Self {
+    pub fn new(row: usize, col: usize) -> Self {
         Position { row, col }
     }
 }
 
 #[derive(Debug)]
-pub struct AreaPosition {
+pub struct CornerPositions {
     //4 corners of the room
     top_left: Position,
     bottom_left: Position,
@@ -40,18 +41,71 @@ pub struct AreaPosition {
     bottom_right: Position,
 }
 
-impl AreaPosition {
+impl CornerPositions {
     pub fn new(
         top_left: Position,
         bottom_left: Position,
         top_right: Position,
         bottom_right: Position,
     ) -> Self {
-        AreaPosition {
+        CornerPositions {
             top_left,
             bottom_left,
             top_right,
             bottom_right,
+        }
+    }
+}
+type VecRangeUSize = std::vec::Vec<std::ops::Range<usize>>;
+pub struct Side {
+    side_points: VecRangeUSize,
+    ref_point: usize,
+    transformed: bool, // transforms rows to cols, cols to rows
+}
+
+// impl Iterator for Side {
+//     type Item = Position;
+//     // Here, we define the sequence using `.curr` and `.next`.
+//     // The return type is `Option<T>`:
+//     //     * When the `Iterator` is finished, `None` is returned.
+//     //     * Otherwise, the next value is wrapped in `Some` and returned.
+//     fn next(&mut self) -> Option<Position> {
+//         let mut side_points = self.side_points.iter();
+
+//         match side_points.next() {
+//             Some(index) if self.transformed => Position::new(row: self.ref_point, index),
+//             Some(index) =>  Position::new(row: index, col: self.ref_point),
+//         };
+//         let val = side_points.next();
+//         if self.transformed{
+//             let position = Position::new(val, 1);
+//             Some(position)
+//         }
+//         else{
+//             Some(Position::new(row: ref_point, col: side_points.next()))
+//         }
+//     }
+
+#[derive(Debug)]
+pub struct CardinalSides {
+    left: (VecRangeUSize, usize),
+    right: (VecRangeUSize, usize),
+    bottom: (usize, VecRangeUSize),
+    top: (usize, VecRangeUSize),
+}
+
+impl CardinalSides {
+    fn new(
+        left: (VecRangeUSize, usize),
+        right: (VecRangeUSize, usize),
+        bottom: (usize, VecRangeUSize),
+        top: (usize, VecRangeUSize),
+    ) -> Self {
+        CardinalSides {
+            left,
+            right,
+            bottom,
+            top,
         }
     }
 }
@@ -118,27 +172,57 @@ pub enum Cell {
 #[derive(Debug)]
 struct Room {
     room_number: i16,
-    global_position: AreaPosition, //position relative to some overlying feature
+    corner_positions: CornerPositions, //position relative to some overlying feature
+    cardinal_sides: CardinalSides,
     hall_cells: Vec<Cell>,
     filled_cells: Vec<Cell>,
     empty_cells: Vec<Cell>,
-    cardinal_bounds: [bool; 4],
-    corner_bounds: [bool; 4],
     // map: RefCell<Weak<Map>>, //This will be a weak reference
 }
 
 impl<'a> Room {
     fn create_seed(size: &'a Size, top_left: Position) -> Self {
-        top_left.row 
+        let corner_positions = Room::create_corner_positions(size, top_left);
+        let sides = Room::create_sides(&corner_positions);
         Room {
-            room_number: 10,
+            room_number: 0,
             hall_cells: vec![],
             filled_cells: vec![],
             empty_cells: vec![],
+            corner_positions: corner_positions,
+            cardinal_sides: sides,
         }
     }
-    fn create_halls_from_room(room: &Room) {
-        let halls = &room.hall_cells;
+    fn create_sides(corners: &CornerPositions) -> CardinalSides {
+        let top = (
+            corners.top_left.row,
+            vec![corners.top_left.col..corners.top_right.col],
+        );
+        let bottom = (
+            corners.bottom_left.row,
+            vec![corners.bottom_left.col..corners.bottom_right.col],
+        );
+        let left = (
+            vec![corners.top_left.row..corners.bottom_left.row],
+            corners.bottom_left.col,
+        );
+        let right = (
+            vec![corners.top_left.row..corners.bottom_left.row],
+            corners.bottom_right.col,
+        );
+        CardinalSides::new(left, right, bottom, top)
+    }
+
+    // fn spawn_potential_hall_cells(&self) -> Vec<HallwayCell> {
+    //     let halls = filte
+    // }
+
+    fn create_corner_positions(size: &'a Size, top_left: Position) -> CornerPositions {
+        let top_right = Position::new(top_left.row + size.row_size, top_left.col);
+        let bottom_left = Position::new(top_left.row, top_left.col + size.col_size);
+        let bottom_right =
+            Position::new(top_left.row + size.row_size, top_left.col + size.col_size);
+        CornerPositions::new(top_left, bottom_left, top_right, bottom_right)
     }
 }
 
@@ -149,32 +233,32 @@ pub struct Maze<'a> {
     all_cells: Vec<Vec<Cell>>,
 }
 
-pub fn empty_cell_vecs(map_size: &Size) -> Vec<Vec<Cell>> {
-    let mut grid: Vec<Vec<Cell>> = vec![];
-    let mut row: Vec<Cell>;
-    let mut cell: EmptyCell;
-    let mut pos: Position; // we want to copy these values into some grid
-
-    for i in 0..map_size.row_size {
-        row = vec![];
-        for j in 0..map_size.col_size {
-            pos = Position::new(i, j);
-            cell = EmptyCell::new(pos);
-            row.push(Cell::Empty(cell));
-        }
-        grid.push(row);
-    }
-    grid
-}
-
 impl<'a> Maze<'a> {
     pub fn new(map_size: &'a Size, is_populated: bool) -> Self {
-        let empty_maze: Vec<Vec<Cell>> = empty_cell_vecs(map_size);
+        let empty_maze: Vec<Vec<Cell>> = Maze::empty_cell_vecs(map_size);
         Maze {
             rooms: vec![],
             map_size,
             all_cells: empty_maze,
         }
+    }
+
+    pub fn empty_cell_vecs(map_size: &Size) -> Vec<Vec<Cell>> {
+        let mut grid: Vec<Vec<Cell>> = vec![];
+        let mut row: Vec<Cell>;
+        let mut cell: EmptyCell;
+        let mut pos: Position; // we want to copy these values into some grid
+
+        for i in 0..map_size.row_size {
+            row = vec![];
+            for j in 0..map_size.col_size {
+                pos = Position::new(i, j);
+                cell = EmptyCell::new(pos);
+                row.push(Cell::Empty(cell));
+            }
+            grid.push(row);
+        }
+        grid
     }
 
     pub fn in_bounds(&self) {}
@@ -184,5 +268,8 @@ impl<'a> Maze<'a> {
 
 fn main() {
     let size = Size::new(255, 255);
+    let position = Position::new(0, 0);
+    let rect_size = Size::new(15, 10);
+    let room = Room::create_seed(&rect_size, position);
     let maze = Maze::new(&size, false);
 }
